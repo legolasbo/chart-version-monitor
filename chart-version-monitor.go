c39e3041caa84a927debd73c7dd56204cba2ab78
Chris Jansen <Chris Jansen>
2022-10-03 11:18:11 +0200

    Move RepositoryContents to repositoryContents.go

--- chart-version-monitor.go
+++ chart-version-monitor.go
@@ -17,52 +17,6 @@ import (
 	"time"
 )
 
-type RepositoryContents struct {
-	Entries   map[string][]Entry `yaml:"entries"`
-	Versions  map[string]semver.Collection
-	Generated string `yaml:"generated"`
-	URL       string
-}
-
-type Entry struct {
-	Version string `yaml:"version"`
-}
-
-func (rc *RepositoryContents) filterCharts(chartsToKeep []Chart) {
-	filtered := make(map[string][]Entry)
-
-	for _, chart := range chartsToKeep {
-		if entries, ok := rc.Entries[chart.Name]; ok {
-			filtered[chart.Name] = entries
-			continue
-		}
-		rc.Entries[chart.Name] = make([]Entry, 0)
-	}
-
-	rc.Entries = filtered
-}
-
-func (rc *RepositoryContents) entriesToVersions() {
-	if rc.Versions == nil {
-		rc.Versions = make(map[string]semver.Collection)
-	}
-	for k, e := range rc.Entries {
-		versions := make(semver.Collection, len(e))
-		for i, v := range e {
-			version, err := semver.NewVersion(v.Version)
-			if err != nil {
-				log.Printf("Error parsing version: %s", err)
-				continue
-			}
-
-			versions[i] = version
-		}
-
-		sort.Sort(versions)
-		rc.Versions[k] = versions
-	}
-}
-
 func main() {
 	config := getConfig()
 	fixRepoURLS(config)
